1ffea597534be728a7a74fc7aa5227070d8c35b5
dev <dev.camelot.ansh@gmail.com>
2022-07-28 11:38:34 +0200

    First Commit

---
+++ src/GitFinalDemo_2/GitFinalDemo_2.java
@@ -0,0 +1,10 @@
+package GitFinalDemo_2;
+
+public class GitFinalDemo_2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,2 @@
+module GitFinalDemo_2 {
+}
